6e634246ccc5326ffdf18296f52e1caa74bc9b95
dzondra <djole.nic@gmail.com>
2021-09-22 18:50:19 +0200

    Sorting | Quick sort

---
+++ Algorithms/Interface/Algorithm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Interface
+{
+    public interface IAlgorithm
+    {
+        public void Execute();
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.Sorting;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    class Program
+    {
+        public static QuickSort quickSort = new QuickSort();
+        // Driver Code
+        public static void Main(String[] args)
+        {
+            quickSort.Execute();
+        }
+    }
+
+}
