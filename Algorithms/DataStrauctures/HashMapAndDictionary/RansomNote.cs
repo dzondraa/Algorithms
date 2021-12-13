9e34fa6ec0379a0c45f4d9863d6e9419e6553592
dzondra <djole.nic@gmail.com>
2021-12-13 17:02:02 +0100

    Ransom Note -> https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps

---
+++ Algorithms/DataStrauctures/HashMapAndDictionary/RansomNote.cs
@@ -0,0 +1,45 @@
+﻿using Algorithms.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DataStrauctures.HashMapAndDictionary
+{
+    public class RansomNote : IAlgorithm
+    {
+        public void Execute()
+        {
+            // Simply use mapMagazine HashMap (Dictionary) for words count.
+            // If there is no enough words to create a note. Write "No"
+            // Otherwise "Yes"
+
+            // *** GIVEN INPUT
+            var magazine = new List<string> { "give", "me", "one", "grand", "today", "night" };
+            var note = new List<string> { "give", "one", "grand", "today" };
+
+            Dictionary<string, int> mapMagazine = new Dictionary<string, int>();
+
+            foreach (string str in magazine)
+            {
+                if (mapMagazine.ContainsKey(str)) mapMagazine[str]++;
+                else mapMagazine.Add(str, 1);
+            }
+
+            foreach (string str in note)
+            {
+                if (!mapMagazine.ContainsKey(str))
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+                if (--mapMagazine[str] == -1)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -13,7 +13,7 @@ namespace Algorithms
 {
     class Program
     {
-        public static IAlgorithm algorithm = new CountTriplets();
+        public static IAlgorithm algorithm = new RansomNote();
         // Driver Code
         public static void Main(String[] args)
         {
