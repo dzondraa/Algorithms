43ace6f8a6018a285462a6df9ac962982375e551
dzondra <djole.nic@gmail.com>
2021-12-20 09:18:43 +0100

    Alternating Characters -> https://www.hackerrank.com/challenges/alternating-characters/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings

---
+++ Algorithms/StringManipulation/AlternatingCharacters.cs
@@ -0,0 +1,24 @@
+﻿using Algorithms.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.StringManipulation
+{
+    public class AlternatingCharacters : IAlgorithm
+    {
+        public void Execute()
+        {
+            // input string -> s
+            string s = "ABABABAB";
+            int count = 0;
+            char previous = 'C';
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == previous) count++;
+                else previous = s[i];
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
