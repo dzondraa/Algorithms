f0fc08934f44f2d925ce7aeee7e2045d37ca28c8
dzondra <djole.nic@gmail.com>
2021-12-20 09:32:17 +0100

    Making Anagrams (https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings)

---
+++ Algorithms/StringManipulation/MakingAnagrams.cs
@@ -0,0 +1,39 @@
+﻿using Algorithms.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.StringManipulation
+{
+    public class MakingAnagrams : IAlgorithm
+    {
+        public void Execute()
+        {
+            // input strings -> (a,b)
+            string a = "cde";
+            string b = "abc";
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int result = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (map.ContainsKey(a[i]))
+                    map[a[i]]++;
+                else map.Add(a[i], 1);
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (map.ContainsKey(b[i]))
+                    map[b[i]]--;
+                else map.Add(b[i], -1);
+            }
+
+            foreach (var kv in map)
+            {
+                result += Math.Abs(kv.Value);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
