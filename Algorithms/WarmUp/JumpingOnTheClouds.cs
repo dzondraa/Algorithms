5ce151b5728df873d91bd35829fad4d7fc5094f4
dzondra <djole.nic@gmail.com>
2021-12-20 16:15:54 +0100

    JumpingOnTheClouds -> https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

---
+++ Algorithms/WarmUp/JumpingOnTheClouds.cs
@@ -0,0 +1,35 @@
+﻿using Algorithms.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.WarmUp
+{
+    class JumpingOnTheClouds : IAlgorithm
+    {
+        public void Execute()
+        {
+            // input -> c
+            var c = new List<int> { 0, 0, 1, 0, 0, 1, 0, };
+
+            int current = 0;
+            int count = 0;
+            while (current < c.Count - 1)
+            {
+                if (current >= c.Count - 2)
+                {
+                    count++;
+                    break;
+                }
+                Console.WriteLine("CURRENT : " + current);
+                if (c[current + 2] != 1)
+                    current += 2;
+                else
+                    current += 1;
+
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
