f64dddd8d2022563ae7afea4935c0675f2d059d3
dzondra <djole.nic@gmail.com>
2022-01-27 20:56:59 +0100

    Repeated String -> https://www.hackerrank.com/challenges/repeated-string/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

---
+++ Algorithms/WarmUp/RepeatedString.cs
@@ -0,0 +1,32 @@
+﻿using Algorithms.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.WarmUp
+{
+    public class RepeatedString : IAlgorithm
+    {
+        public void Execute()
+        {
+            // abaabaaba|a
+            // -> 'a' after | is offset (the characters that left after the deviding to chunks
+            // -> we know how many repetitions 1 chunk has
+            // -> final forumla -> oneChunkCount * (n / s.Length) + offsetCount
+
+            int n = 10;
+            string s = "aba";
+
+            long offset = n % s.Length;
+            long oneChunkCount = 0, offsetCount = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a') oneChunkCount++;
+                if (s[i] == 'a' && i < offset) offsetCount++;
+            }
+
+            Console.WriteLine("Total: " + (oneChunkCount * (n / s.Length) + offsetCount));
+        }
+    }
+}
